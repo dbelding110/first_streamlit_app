76d7635fda864cded9c590465faefb1b0f83aab4
dbelding110 <davebelding@hotmail.com>
2022-08-25 10:19:15 -0700

    Update streamlit_app.py
    added breakfast items

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,6 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
+streamlit.text('Oatmeal')
+streamlit.text('Kale Smoothie')
+streamlit.text('hard boiled egg')
