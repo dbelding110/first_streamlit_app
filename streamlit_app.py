18f5855aa9491cebfd3673e4bfb916be719e0d08
dbelding110 <davebelding@hotmail.com>
2022-08-25 10:20:13 -0700

    Update streamlit_app.py
    changed items

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Oatmeal')
-streamlit.text('Kale Smoothie')
-streamlit.text('hard boiled egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
